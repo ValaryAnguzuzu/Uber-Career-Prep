d9d7ca28715028aa36426167cce1c2b2629e760c
Valary Anguzuzu <anguzuzuvalary@gmail.com>
2025-06-06 12:32:23 -0500

    uber hw2

---
+++ homework1/homework2/SinglyLinkedList.py
@@ -0,0 +1,15 @@
+Class ListNode: #DEFINE OUR NODE STRUCTURE
+    def __init__(self,val): #attributes
+        self.val = val
+        self.next = None
+
+def insertAtFront(self, val: int ) -> None:
+    """"
+    create the new node; set its next ptr to original head and update head to point to this new node
+    """"
+    node = ListNode(val)
+    node.next = self.head
+
+
+    
+
